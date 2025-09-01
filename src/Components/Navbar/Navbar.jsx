efbb0bf89586ac7dc44fd39fee911d731b635849
Mohamed Asran <78270356+MohamedAsran0@users.noreply.github.com>
2025-09-01 18:01:08 +0300

    add upload profile photo feature

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
     <>
 
 
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed w-full">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed w-full z-10">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={logo} className="h-8" alt="Flowbite Logo" />
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { tokenContextObj } from '../../Context/TokenContextProvider'
 import { RiseLoader } from 'react-spinners'
 import PostCard from '../PostCard/PostCard'
 import { jwtDecode } from "jwt-decode";
+import { toast } from 'react-toastify'
 
 
 export default function Profile() {
@@ -12,6 +13,10 @@ export default function Profile() {
   const { token } = useContext(tokenContextObj);
 
   const decoded = jwtDecode(token);
+
+  const imageUpload = useRef(null);
+
+  const qClient = useQueryClient();
 
 
   function getUserPosts() {
@@ -27,6 +32,38 @@ export default function Profile() {
     queryKey: ['getUserPosts'],
     queryFn: getUserPosts,
   })
+
+  const { isPending: isUploadImagePending, mutate: mutateHandleImageUploaded } = useMutation({
+    mutationFn: handleImageUploaded,
+
+    onSuccess: () => {
+      imageUpload.current.value = '';
+      toast.success('Image updated successfully');
+      qClient.invalidateQueries(['getUserPosts']);
+    },
+
+    onError: (err) => {
+      toast.error(err.message || 'Error uploading image');
+    }
+  })
+
+
+  function handleImageUploaded() {
+    const formData = new FormData();
+
+    if (imageUpload.current.value == '') {
+      throw new Error('Cannot Upload Empty Photo');
+    }
+    if (imageUpload.current.value != '') {
+      formData.append('photo', imageUpload.current.files[0]);
+    }
+
+    return axios.put('https://linked-posts.routemisr.com/users/upload-photo', formData, {
+      headers: {
+        token: token
+      }
+    })
+  }
 
 
   if (isLoading) {
@@ -60,18 +97,26 @@ export default function Profile() {
 
   return (
     <>
+      {isUploadImagePending && <div className='fixed top-20 left-1/2'>
+        <RiseLoader color="#0f4ff1" />
+      </div>}
+
+
       <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8 mx-10">
         <div className='h-70 w-full bg-gray-600 pt-12 mb-35'>
-          <div className='bg-white w-fit rounded-full ml-5 relative cursor-pointer'>
-            <div className='w-full rounded-full overflow-hidden'>
-              <img src={data.data.posts?.[0].user.photo} className='w-full' alt="User Photo" />
+          <label htmlFor="uploadProfilePhoto">
+            <div className='bg-white w-2/3 md:w-1/2 lg:w-1/4 rounded-full ml-5 relative cursor-pointer'>
+              <div className='w-full rounded-full overflow-hidden'>
+                <img src={data.data.posts?.[0].user.photo} className='w-full' alt="User Photo" />
+              </div>
+              <div className='absolute w-14 h-14 right-3 bottom-3 p-3 rounded-full bg-green-300 text-2xl font-semibold text-center'>+</div>
             </div>
-            <div className='absolute w-14 h-14 right-3 bottom-3 p-3 rounded-full bg-green-300 text-2xl font-semibold text-center'>+</div>
-          </div>
+          </label>
+          <input ref={imageUpload} onChange={mutateHandleImageUploaded} type="file" id="uploadProfilePhoto" hidden />
           <h1 className='text-4xl p-7 pl-32 font-bold'>{data.data.posts?.[0].user.name}</h1>
         </div>
 
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-xl flex flex-col gap-4">
+        <div className="mt-40 sm:mx-auto sm:w-full sm:max-w-xl flex flex-col gap-4">
           {data.data.posts.map((post) => (
             <PostCard key={post._id} postDetails={post} postUser={post.user} />
           ))}
